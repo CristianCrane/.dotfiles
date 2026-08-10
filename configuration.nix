24d739e388d7181146aa6082e257a023a7b443a7
Cris Crane <12250662+CristianCrane@users.noreply.github.com>
2026-08-10 18:27:20 +0000

    enable dynamic linking support

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,9 @@
 	# nixos specific settings
 	wsl.enable = true;
 	wsl.defaultUser = "cris";
+
+        # enables automatic dynamic linking support for unpatched binaries in $PATH / node
+        programs.nix-ld.enable = true;
 	
 	# general system settings
 	system.stateVersion = "26.05";
