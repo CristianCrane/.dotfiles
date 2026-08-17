19031e3f9e627279c65eb31c23adab5705fd88f8
Cris Crane <12250662+CristianCrane@users.noreply.github.com>
2026-08-17 13:38:49 +0000

    update nvim theme to darcula-dark

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "xiantang/darcula-dark.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("darcula").setup({})
+      vim.cmd.colorscheme("darcula-dark")
+    end,
+  },
+  {
     'folke/which-key.nvim',
     lazy = false,
     config = true -- popup that shows what my leader keys do
