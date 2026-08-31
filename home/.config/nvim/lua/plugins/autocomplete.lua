911c9e14a3c0d4976a2752efdd7b22707ecadea5
Cris Crane <12250662+CristianCrane@users.noreply.github.com>
2026-08-31 17:41:41 +0000

    update blink.cmp nvim plugin to latest docs

--- home/.config/nvim/lua/plugins/autocomplete.lua
+++ home/.config/nvim/lua/plugins/autocomplete.lua
@@ -1,34 +1,25 @@
 return {
-  "Saghen/blink.cmp",
-  -- Download pre-built Rust fuzzy matcher binaries from releases
-  version = "v0.*",
-  event = "InsertEnter",
+  "saghen/blink.cmp",
+  dependencies = {
+    'rafamadriz/friendly-snippets'
+  },
+  version = "1.*",
   opts = {
-    -- Keymap preset: 'default' | 'super-tab' | 'enter'
-    -- 'default': <C-space> to show, <Tab>/<S-Tab> or <C-n>/<C-p> to navigate, <C-y> to accept
-    keymap = { preset = "default" },
-
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
+    keymap = {
+      preset = "enter",
     },
-
-    -- Completion behavior & triggers
     completion = {
-      -- Show completion documentation popup automatically when navigating items
-      documentation = { auto_show = true, auto_show_delay_ms = 200 },
-
-      -- Render kind icons (Method, Variable, Interface, etc.) in popup
+      documentation = { auto_show = true, auto_show_delay_ms = 150 },
       menu = {
         draw = {
           columns = { { "kind_icon" }, { "label", "label_description", gap = 1 } },
         },
       },
     },
-
-    -- Completion sources (LSP + Snippets + Path + Buffer)
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
   },
+  opts_extend = { "sources.default" }
 }
