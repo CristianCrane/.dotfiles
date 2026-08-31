7e642bcfb45fbd46727db5e3613b0272433d08d4
Cris Crane <12250662+CristianCrane@users.noreply.github.com>
2026-08-31 17:43:23 +0000

    add lspsaga nvim plugin

---
+++ home/.config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,26 @@
+return {
+  "nvimdev/lspsaga.nvim",
+  event = "LspAttach",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("lspsaga").setup({
+      hover = {
+        max_width = 0.8,
+        open_link = "gx",
+      },
+      ui = {
+        border = "rounded",
+      },
+      lightbulb = {
+        enable = false,
+      },
+      diagnostic = {
+        show_code_action = true,
+        jump_num_shortcut = true,
+      },
+    })
+  end,
+}
