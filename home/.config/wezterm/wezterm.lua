6d314b9763bebc51b8004f540f46d4bdcfa3a7a0
Cris Crane <12250662+CristianCrane@users.noreply.github.com>
2026-08-31 17:27:56 +0000

    shiftenter passthrough in wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -14,9 +14,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
 config.keys = {
-  -- Forward Ctrl+Tab and Ctrl+Shift+Tab to terminal applications
-  { key = 'Tab', mods = 'CTRL',       action = wezterm.action.SendString '\x1b[27;5;9~' },
-  { key = 'Tab', mods = 'CTRL|SHIFT', action = wezterm.action.SendString '\x1b[27;6;9~' },
+  { key = 'Tab',   mods = 'CTRL',       action = wezterm.action.SendString '\x1b[27;5;9~' },
+  { key = 'Tab',   mods = 'CTRL|SHIFT', action = wezterm.action.SendString '\x1b[27;6;9~' },
+  { key = 'Space', mods = 'CTRL',       action = wezterm.action.SendKey { key = 'Space', mods = 'CTRL' }, },
+  { key = 'Enter', mods = 'SHIFT',      action = wezterm.action.SendString('\27[13;2u') },
 }
 
 return config
